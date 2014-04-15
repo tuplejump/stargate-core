62556c9687ae9e6e66cf3bad86384336bb1d33f3
Satya <satya@tuplejump.com>
2014-04-15 06:33:58 +0530

    Perf comparision in test

--- src/test/java/com/tuplejump/stargate/perf/PerfSGTest.java
+++ src/test/java/com/tuplejump/stargate/perf/PerfSGTest.java
@@ -25,12 +25,17 @@ public class PerfSGTest extends IndexTestBase {
                 String state = states[i];
                 countResults("TAG3", "state = '" + state + "'", false);
             }
+            for (int i = 0; i < states.length; i++) {
+                String state = states[i];
+                countResults("TAG3", "state1 = '" + state + "'", false);
+            }
+
             System.out.println("--------Cache primed-----");
             countResults("TAG3", "state = 'NONEXISTENT'", false);
-            for (int i = 0; i < 10; i++) {
+            /*for (int i = 0; i < 5; i++) {
                 String state = getRandomState();
                 countResults("TAG3", "state = '" + state + "'", false);
-            }
+            }*/
         } finally {
             dropTable(keyspace, "TAG3");
             dropKS(keyspace);
@@ -40,31 +45,32 @@ public class PerfSGTest extends IndexTestBase {
 
     private void createTableAndIndexForRow() {
         getSession().execute("USE " + keyspace + ";");
-        getSession().execute("CREATE TABLE TAG3(key int primary key, tags varchar, state varchar)");
+        getSession().execute("CREATE TABLE TAG3(key int primary key, tags varchar, state varchar, state1 varchar)");
         int i = 0;
         long r5k = System.nanoTime();
-        while (i < 40000) {
+        while (i < 12000) {
             long rowsTime = System.nanoTime();
-            if (i == 20000) {
+            if (i == 6000) {
                 getSession().execute("CREATE CUSTOM INDEX stateindex ON TAG3(state) USING 'com.tuplejump.stargate.cas.PerColIndex'");
+                getSession().execute("CREATE INDEX stateindex1 ON TAG3(state1)");
                 System.out.println("-------- Created Index ---------");
             }
 
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 1) + ",'hello1 tag1 lol1', 'CA')");
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 2) + ",'hello1 tag1 lol2', 'LA')");
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 3) + ",'hello1 tag2 lol1', 'NY')");
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 4) + ",'hello1 tag2 lol2', 'TX')");
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 5) + ",'hllo3 tag3 lol3', 'TX')");
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 6) + ",'hello2 tag1 lol1', 'CA')");
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 7) + ",'hello2 tag1 lol2', 'NY')");
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 8) + ",'hello2 tag2 lol1', 'CA')");
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 9) + ",'hello2 tag2 lol2', 'TX')");
-            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state) values (" + (i + 10) + ",'hllo3 tag3 lol3', 'TX')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 1) + ",'hello1 tag1 lol1', 'CA','CA')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 2) + ",'hello1 tag1 lol2', 'LA','LA')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 3) + ",'hello1 tag2 lol1', 'NY','NY')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 4) + ",'hello1 tag2 lol2', 'TX','TX')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 5) + ",'hllo3 tag3 lol3' , 'TX','TX')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 6) + ",'hello2 tag1 lol1', 'CA','CA')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 7) + ",'hello2 tag1 lol2', 'NY','NY')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 8) + ",'hello2 tag2 lol1', 'CA','CA')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 9) + ",'hello2 tag2 lol2', 'TX','TX')");
+            getSession().execute("insert into " + keyspace + ".TAG3 (key,tags,state,state1) values (" + (i + 10) + ",'hllo3 tag3 lol3', 'TX','TX')");
             i = i + 10;
-            if (i % 5000 == 0) {
+            if (i % 3000 == 0) {
                 double taken = (rowsTime - r5k) / 1000000;
                 r5k = rowsTime;
-                System.out.println("Time taken for 5k is -" + taken + " milli seconds");
+                System.out.println("Time taken for 2k is -" + taken + " milli seconds");
             }
         }
     }
